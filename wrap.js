cc69f05df65757fc31f31a86f6cf3ed375fb00e7
Kevin Gil <93kevingil@gmail.com>
2021-03-08 22:42:01 -0500

    more wrap work

--- wrap.js
+++ wrap.js
@@ -1,14 +1,16 @@
 const wrap = (line, maxLength) => {
-  const arrLine = line.split("");
-  const returnArr = [];
+  const arrLine = line.split('');
+  let returnArr = [];
   for (let i = 0; i < arrLine.length; i++) {
     if ((i + 1) % maxLength === 0) {
       const newArr = arrLine.slice(0, i);
-      const idxOfSpace = newArr.find(" ");
-      newArr.splice(idxOfSpace, "\n");
-      returnArr.concat(newArr);
+      const reverse = newArr.reverse();
+      const idxOfSpace = reverse.indexOf(' ');
+      reverse.splice(idxOfSpace, 0, '\n');
+      const backToNormal = reverse.reverse();
+      returnArr = returnArr.concat(backToNormal);
     }
-    return returnArr.join("");
   }
+  return returnArr.join('');
 };
 module.exports = wrap;
